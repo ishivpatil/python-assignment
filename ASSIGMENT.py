5ecc5f7cdf515acf177b94ff5a7d79220455beed
ishivpatil <shivprasadmundkar3@gmail.com>
2025-05-16 10:26:46 +0530

    add code for complete assignment

---
+++ ASSIGMENT.py
@@ -0,0 +1,20 @@
+#task 1
+#taking two number as input from the user
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+print("you entered:",num1,"and",num2)
+# ADDITION IN PYTHON5
+print(num1+num2)
+#substraction in python
+print(num1-num2)
+#multiplication in python
+print(num1*num2)
+#division in python5
+print(num1/num2)
+#Task 2: Create a Personalized Greeting
+#Take  first and last name as input
+first_name=(input("Enter first name: "))
+last_name=(input("Enter last name: "))
+#Concatenate the first name and last name
+full_name = first_name + "" + last_name
+print(f"\nHello, {full_name}! Welcome to the  Python program.!")
